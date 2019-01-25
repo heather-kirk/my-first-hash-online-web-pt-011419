9c7294c4f651feb4e483daca33b0af5aa10e2fde
Heather Kirk <hskirk@gmail.com>
2019-01-25 03:49:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+puts shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -34,12 +34,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
-
+shipping_manifest["muskets"] = 2
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-
+puts shipping_manifest
 end
